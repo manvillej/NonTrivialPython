56f5197f94060e9081ebd5c2b06eade907bfa144
manvillej <jeffmanvillejr@gmail.com>
2017-10-27 16:27:25 -0400

    Updated Function 2-4 to PEP8
    updated Function2-4 to PEP8 standards

--- Function2.py
+++ Function2.py
@@ -1,24 +1,25 @@
 def main():
-	# returns 200:
-	mymul('foo', 'bar', 10, 20)
+    # returns 200:
+    mymul('foo', 'bar', 10, 20)
 
-	# returns 1:
-	mymul()
+    # returns 1:
+    mymul()
 
-	# returns 7:
-	mymul(7)
+    # returns 7:
+    mymul(7)
+
 
 def mymul(*args):
-	result = 1
+    result = 1
 
-	for arg in args:
-		try:
-			result = result*int(arg)
-		except ValueError:
-			pass
-	    	# it was a string, not an int.
+    for arg in args:
+        try:
+            result = result*int(arg)
+        except ValueError:
+            pass
+            # it was a string, not an int.
 
-	print(result)
+    print(result)
 
 if __name__ == '__main__':
-	main()
+    main()
